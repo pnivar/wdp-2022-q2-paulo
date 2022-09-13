774f780c0922f48cc5869e871be93974edf2e7fb
pnivar <nivarpaulo@gmail.com>
2022-09-12 20:22:08 -0400

    1-basic-exercise-1.js commit

---
+++ my-work/3-js/1-basic-exercise-1/1-basic-exercise-1.js
@@ -0,0 +1,14 @@
+// Exercise 1.
+let myName = 'Paulo Nivar';
+let intro = 'Hello, my name is ' + myName;
+console.log(intro);
+
+// Exercise 2.
+let x = 1;
+let y =2;
+
+// I'm sure there are many ways to accomplish this task, is there a proper way to accomplish this task?
+x = x + x;
+y = y / y;
+
+

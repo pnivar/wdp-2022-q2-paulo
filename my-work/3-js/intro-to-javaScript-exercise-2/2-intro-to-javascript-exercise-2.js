44b294ec13b100a4309d0db036824efaece0ee4c
pnivar <nivarpaulo@gmail.com>
2022-09-16 17:40:41 -0400

    intro-to-javascript-exercise-2 commit

---
+++ my-work/3-js/intro-to-javaScript-exercise-2/2-intro-to-javascript-exercise-2.js
@@ -0,0 +1,27 @@
+// Ex. 1
+/*
+let x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+*/
+
+// x should have the last assigned value which is equal to 'Toe'.
+
+//Ex. 2
+/*
+let x = 'Laurel';
+let y = 'Hardy';
+let z = y;
+y = x;
+x = z;
+*/
+
+// the value of x is equal to y which corresponds to 'Hardy'
+
+//ex. 3
+const groceryList = ['apples', 'oranges', 'bananas', 'pears', 'lemons'];
+const groceryPrices = ['$0.50', '$0.75','$0.95','$0.50','$0.89'];
+
+for (let i = 0; i < groceryList.length; i++) {
+    console.log(groceryList[i] + " " + groceryPrices[i]);
+}
